43375d5eb8d1d32c14aebcec19f45e38d5b9d058
Federico Rossi <federicorossi888@gmail.com>
2025-07-19 16:08:00 -0300

    update

--- src/yaml_to_html.py
+++ src/yaml_to_html.py
@@ -6,23 +6,21 @@ def load_yaml(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return yaml.safe_load(file)
 
-def format_date(date_dict):
-    if not date_dict:
-        return ""
-    if 'start' in date_dict and 'end' in date_dict:
-        return f"{date_dict['start']} - {date_dict['end']}"
-    elif 'year' in date_dict:
-        return f"{date_dict['year']}"
-    return ""
+def merge_dicts(a, b):
+    # Recursively merge two dicts (b overrides a)
+    result = a.copy()
+    for k, v in b.items():
+        if k in result and isinstance(result[k], dict) and isinstance(v, dict):
+            result[k] = merge_dicts(result[k], v)
+        else:
+            result[k] = v
+    return result
 
 def render_html(data, template_path):
     env = Environment(loader=FileSystemLoader(os.path.dirname(template_path)))
     template = env.get_template(os.path.basename(template_path))
 
-    # Subtitle as a multiline string
     subtitle = "<br>".join(data.get('subtitle', []))
-
-    # Process experiences
     experiences = []
     for exp in data.get('experiences', []):
         exp = exp.copy()
@@ -33,7 +31,6 @@ def render_html(data, template_path):
         exp['title'] = exp.get('title', '')
         experiences.append(exp)
 
-    # Process other sections (projects, education, etc.)
     projects = data.get('projects', [])
     education = data.get('education', [])
     contact = data.get('contact', {})
@@ -58,15 +55,17 @@ def save_html(output_path, html_content):
         file.write(html_content)
 
 def main():
-    yaml_file = 'data/resume.yaml'
-    yaml_file_esp = 'data/resume-esp.yaml'
+    common_file = 'data/common.yml'
+    eng_file = 'data/content-eng.yml'
+    esp_file = 'data/content-esp.yml'
     template_file = 'src/template/resume_template.html'
     output_file = 'docs/index.html'
     output_file_esp = 'docs/index-esp.html'
 
-    data = load_yaml(yaml_file)
-    data_esp = load_yaml(yaml_file_esp)
-    html_content = render_html(data, template_file)
+    common = load_yaml(common_file)
+    data_eng = merge_dicts(common, load_yaml(eng_file))
+    data_esp = merge_dicts(common, load_yaml(esp_file))
+    html_content = render_html(data_eng, template_file)
     html_content_esp = render_html(data_esp, template_file)
     save_html(output_file, html_content)
     save_html(output_file_esp, html_content_esp)
